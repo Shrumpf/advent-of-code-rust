0042824d6ff94cf54105b50835f339ff76dd618d
Shrumpf <flori.voss@gmx.net>
2022-12-01 13:04:12 +0100

    2022 - Day 01

--- 2022/src/solutions/day_01.rs
+++ 2022/src/solutions/day_01.rs
@@ -4,24 +4,39 @@ pub struct Day01 {}
 
 impl Solution for Day01 {
     fn name(&self) -> String {
-        "TODO".to_owned()
+        "Calorie Counting".to_owned()
     }
 
-    fn part_a(&self) -> i32 {
-        let numbers = common::load("01")
-            .lines()
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
-
-        0
+    fn part_a(&self) -> String {
+        common::load("01")
+            .split("\n\n")
+            .map(|f| {
+                f.split('\n')
+                    .map(|c| c.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>()
+            })
+            .map(|s| s.iter().sum())
+            .collect::<Vec<u32>>()
+            .iter()
+            .max()
+            .unwrap()
+            .to_string()
     }
 
-    fn part_b(&self) -> i32 {
-        let numbers = common::load("01")
-            .lines()
-            .map(|x| x.parse::<u32>().unwrap())
+    fn part_b(&self) -> String {
+        let mut numbers: Vec<u32> = common::load("01")
+            .split("\n\n")
+            .map(|f| {
+                f.split('\n')
+                    .map(|c| c.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>()
+            })
+            .map(|s| s.iter().sum())
             .collect::<Vec<u32>>();
+        numbers.sort();
+        numbers.reverse();
+        let max = vec![numbers[0], numbers[1], numbers[2]].iter().sum::<u32>();
 
-        0
+        max.to_string()
     }
 }
